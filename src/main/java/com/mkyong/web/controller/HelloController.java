7835e442d22f1dd51a6766f8e35edc14fb41e6ae
Tanmaya Nayak <tanmaya.nayak.kumar@gmail.com>
2022-01-11 00:32:34 +0530

    updated

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -13,8 +13,8 @@ public class HelloController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 
-		model.addAttribute("message", "Spring 3 MVC Hello TanmayaNayak");
-		return "Hello";
+		model.addAttribute("message", "Spring 3 MVC Hello TanmayaKuNayak");
+		return "hello";
 
 	}
 
